d637e006d1c67ef86cbe9371ba2bf5209ed39d7f
RollViral <51767087+tyler-Github@users.noreply.github.com>
2023-10-05 20:30:24 +0100

    Update notifications.js

--- src/modules/notifications.js
+++ src/modules/notifications.js
@@ -1,3 +1,13 @@
+/*
+ * VMGFrame - notifications.js
+ * Version 1.0.1
+ * https://github.com/VMGWARE/VMGFrame
+ *
+ * (C) 2023 Company VMGWare. All rights reserved.
+ * This code is open source and available under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 window.notifications = function(message) {
     window.showNotification = function(message) {
         const notification = document.createElement('div');
