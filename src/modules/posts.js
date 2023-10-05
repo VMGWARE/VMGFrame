e5d24de5e769bcb18946f72dc79f241dedf2c23a
RollViral <51767087+tyler-Github@users.noreply.github.com>
2023-10-05 20:30:13 +0100

    Update posts.js

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,3 +1,14 @@
+/*
+ * VMGFrame - posts.js
+ * Version 1.0.1
+ * https://github.com/VMGWARE/VMGFrame
+ *
+ * (C) 2023 Company VMGWare. All rights reserved.
+ * This code is open source and available under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+
 window.posts = function () {
     const root = document.getElementById('root');
     root.innerHTML = '';
