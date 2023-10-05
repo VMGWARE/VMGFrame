34748db95ba0d9a6e9a9433340d7e6fd3d99827f
RollViral <51767087+tyler-Github@users.noreply.github.com>
2023-10-05 20:31:58 +0100

    Update footer.js

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -1,3 +1,13 @@
+/*
+ * VMGFrame - footer.js
+ * Version 1.0.1
+ * https://github.com/VMGWARE/VMGFrame
+ *
+ * (C) 2023 Company VMGWare. All rights reserved.
+ * This code is open source and available under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 window.footer = function() {
     const footer = document.createElement('footer');
     footer.classList.add('footer', 'bg-light', 'text-center', 'py-2');
